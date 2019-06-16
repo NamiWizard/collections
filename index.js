f61c099edcc34d003dd22f0345c42c23eb42c038
XiaoYun <yun870204@gmail.com>
2019-06-16 14:26:06 +0800

    add database

---
+++ index.js
@@ -0,0 +1,49 @@
+const Koa = require('koa');
+//koa-static = 服務靜態路由
+const serve = require('koa-static');
+const Router = require('koa-router');
+const BodyParser = require('koa-body');
+
+
+const app = new Koa();
+//Router = 哪一個路徑用甚麼方法做甚麼事
+const router = new Router();
+const bodyParser = new BodyParser();
+
+//資料庫
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: '34.80.150.158',
+    user: 'root',
+    password: 'mp6_0204',
+    database: 'collection',
+    port: 3306
+});
+
+
+
+
+
+//設定路由
+router.post('/comments', bodyParser, function (ctx, next) {
+    let body = ctx.request.body;
+    conn.connect();
+    conn.query(`INSERT INTO comment (name,email,subject,content) VALUES ('${body.name}','${body.email}','${body.subject}','${body.message}')`, function (err) {
+        conn.end();
+        if (err) throw err;
+    });
+
+    ctx.status = 200;
+
+    ctx.body = `your request: ${JSON.stringify(ctx.request.body)}`;
+    next();
+});
+
+//使用上方設定的路由
+app.use(router.routes());
+
+//__dirname = 目前檔案資料夾位置
+//讓koa用在現在檔案資料夾底下的static資料夾
+app.use(serve(__dirname + '/static'));
+
+app.listen(3000);
